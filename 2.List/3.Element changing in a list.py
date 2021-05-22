63a071bd9518e50bf724eba28f14dc810d4ff13b
Md Danish <56966595+danishhx@users.noreply.github.com>
2021-05-22 21:45:56 +0530

    Add files via upload

---
+++ 2.List/3.Element changing in a list.py
@@ -0,0 +1,7 @@
+myList = [1, 2, 3, 4, "Hello", "World"]  # defining a list
+print("List before change is-->", myList)
+myList[0] = 0 # assigning different values
+myList[1] = 1
+myList[-1] = "Answer"
+myList[-2] = "Question"
+print("List after change is-->", myList)
