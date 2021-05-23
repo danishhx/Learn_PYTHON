90e57c6f00e7eb95379f51588c53f2837cbda477
Md Danish <56966595+danishhx@users.noreply.github.com>
2021-05-23 16:38:41 +0530

    Delete 2.List directory

--- 2.List/1. Access lists elements using the index value.py
+++
@@ -1,7 +0,0 @@
-mylist=[1,2,3,4,5,"hello","world"]
-print(mylist)    #printing List
-print(mylist[0]) #printing 1st Element
-print(mylist[1]) #printing 2nd Element
-print(mylist[5]) #printing 6th Element
-print(mylist[-1])#printing last Element
-print(mylist[-2])#Printing 2nd last Element
--- 2.List/3.Element changing in a list.py
+++
@@ -1,7 +0,0 @@
-myList = [1, 2, 3, 4, "Hello", "World"]  # defining a list
-print("List before change is-->", myList)
-myList[0] = 0 # assigning different values
-myList[1] = 1
-myList[-1] = "Answer"
-myList[-2] = "Question"
-print("List after change is-->", myList)
